5990a8d0f96243f343ec99063d1e2b5f44ebe101
JuanSantana99 <juanpatrab99@gmail.com>
2023-02-13 21:58:34 -0700

    se agrega el proyecto

---
+++ android/app/src/main/kotlin/com/example/u1_3_stateless/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.u1_3_stateless
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('ExampleApp'),
+            ),
+            body: Center(
+                child: Text(
+                    '10',
+                    style: TextStyle(
+                        fontSize: 30
+                    ),
+                ),
+            )
+        );
+    }
+}
+
+
